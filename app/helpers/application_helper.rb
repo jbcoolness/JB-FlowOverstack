ecd7f87a5406acaa82d39d65f7e4d352f459627b
Julio Bertty <julio.bertty@gmail.com>
2019-05-29 21:19:07 -0500

    Added navbar with options login and Gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+    def gravatar_url(email, size)
+        gravatar_id = Digest::MD5::hexdigest(email).downcase  
+        default_url = "http://mysite.com/myavatar.png" # Acá puedo poner una ruta para una imagen propia.  
+        default_url = "retro"  
+        url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"  
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'questions/index'
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root "home#index"
+  root "questions#index"
 end
