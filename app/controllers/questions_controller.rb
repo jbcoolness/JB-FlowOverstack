b3244d7139422742802b4984a7ba52ce516da156
Julio Bertty <julio.bertty@gmail.com>
2019-06-08 14:29:17 -0500

    create question, controller and view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,32 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+    @question.user = current_user
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user = current_user
+    
+    if @question.save
+      flash[:notice] = "Pregunta creada correctamente"
+      redirect_to root_path
+    else
+      render :new
+    end  
+  end
+
+
+
+
+
+  private
+  def question_params
+    params.require(:question).permit(:user_id, :title, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-  devise_for :users
+  resources :questions
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
   root "questions#index"
+  
+  
+  devise_for :users
 end
