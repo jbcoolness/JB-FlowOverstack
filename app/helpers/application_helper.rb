d3395a89796256793799aa8d1b490bc199585f05
Julio Bertty <julio.bertty@gmail.com>
2019-06-08 15:41:55 -0500

    added helper flash message of validations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+    def flash_msg(key)
+        case key
+            when 'notice' then 'alert alert-dismissible alert-info'
+            when 'success' then 'alert alert-dismissible alert-success'  
+            when 'error' then 'alert alert-dismissible alert-danger'  
+            when 'alert' then 'alert alert-dismissible alert-warning'
+        end    
+    end
+
     def gravatar_url(email, size)
         gravatar_id = Digest::MD5::hexdigest(email).downcase  
         default_url = "http://mysite.com/myavatar.png" # Acá puedo poner una ruta para una imagen propia.  
