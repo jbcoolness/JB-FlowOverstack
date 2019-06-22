18c0c232306482fd1a8024893b631caef56307a2
Julio Bertty <julio.bertty@gmail.com>
2019-06-22 12:34:24 -0500

    added search question funtionality at the index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 
 
   def index
-    @questions = Question.all
+    @questions = Question.search(params[:search]).order(created_at: :DESC)
   end
 
   def new
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :comments, as: :commentable
+
+  def self.search(search)
+    search ? where(['title LIKE ?', "%#{search}%"]) : all
+  end
+
 end
